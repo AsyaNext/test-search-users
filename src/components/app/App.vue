ab92225669bf580e9bfb161c6dc55297aefc677c
AsyaNext <nastushaburd@mail.ru>
2021-06-13 20:19:05 +0300

    feat: search users and sort

--- src/components/app/App.vue
+++ src/components/app/App.vue
@@ -2,7 +2,34 @@
   <div class="page">
     <div class="page-header">
       <h1>Пользователи GitHub</h1>
+      <div class="page-search">
+        <input type="text" v-model="search" placeholder="Логин">
+        <button :disabled="!search" @click="searchUsers">Найти</button>
+      </div>
     </div>
+    <div v-if="isSearch && users.length > 0" class="page-list">
+      <div class="page-sort">
+        <button @click="sort('up')">По возрастанию</button>
+        <button @click="sort('down')">По убыванию</button>
+      </div>
+      <span><strong>Всего записей: </strong>{{ $store.getters.count }}</span>
+      <div
+          class="page-item"
+          v-for="(user, index) in users"
+          :key="index"
+          @click="selectUser(user.login)"
+      >
+        <span :class="{ 'active-user' : select === user.login }" class="page-item__user">{{ user.id }}. {{ user.login }}</span>
+        <div v-if="select === user.login" class="page-item__info">
+          <span>Количество публичных репозиториев: {{ someUser.public_repos }}</span>
+          <span>Количество подписчиков: {{ someUser.followers }}</span>
+        </div>
+      </div>
+    </div>
+    <div v-else-if="isLoading">...</div>
+    <div v-else-if="(isSearch && users.length === 0) || isWarning" class="page-warning">Ничего не найдено!</div>
+    <div v-else class="page-welcome">Введите в поисковой строке логин пользователя, которого хотите найти!</div>
+    <button v-if="isSearch && users.length > 0 && users.length !== $store.getters.count" class="page-next" @click="$store.dispatch('searchUsers', { search: search, sort: mode, order: order })">Показать ещё</button>
   </div>
 </template>
 
@@ -10,9 +37,57 @@
 export default {
   name: 'App',
   data: () => ({
+    order: '',
+    mode: '',
+    search: '',
+    select: '',
+    isSearch: false,
+    isWarning: false,
+    isLoading: false
   }),
-  created () {
-    this.$store.dispatch('setLog')
+  computed: {
+    users () {
+      return this.$store.getters.users
+    },
+    someUser () {
+      return this.$store.getters.someUser
+    }
+  },
+  methods: {
+    sort (mode) {
+      this.mode = mode
+      if (this.mode === 'up') {
+        this.order = 'asc'
+      } else {
+        this.order = 'desc'
+      }
+      this.$store.commit('REFRESH_STATE')
+      this.$store.dispatch('searchUsers',
+          {
+            search: this.search,
+            sort: this.mode,
+            order: this.order
+          })
+    },
+    selectUser (login) {
+      this.select = ''
+      this.$store.dispatch('getSomeUser', login)
+        .then(() => {
+          this.select = login
+        })
+    },
+    searchUsers () {
+      this.isSearch = true
+      this.isLoading = true
+      this.mode = ''
+      this.order = ''
+      this.$store.commit('REFRESH_STATE')
+      this.$store.dispatch('searchUsers', { search: this.search })
+        .then(() => {
+          this.isLoading = false
+          this.isWarning = this.users.length === 0
+        })
+    }
   }
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -1,15 +1,77 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
+const api = axios.create({
+    baseURL: 'https://api.github.com/'
+})
+
 const store = new Vuex.Store({
-    state: {},
-    getters: {},
-    mutations: {},
+    state: {
+        users: [],
+        someUser: {},
+        page: 1,
+        count: 0
+    },
+    getters: {
+        users: state => state.users,
+        someUser: state => state.someUser,
+        count: state => state.count
+    },
+    mutations: {
+        GET_USERS (state, payload) {
+            state.count = payload.total_count
+            state.users.push(...payload.items)
+            state.page++
+        },
+        GET_SOME_USER (state, payload) {
+          state.someUser = payload
+        },
+        REFRESH_STATE (state) {
+            state.users = []
+            state.page = 1
+            state.count = 0
+        }
+    },
     actions: {
-        setLog () {
-            console.log(true)
+        searchUsers (context, data) {
+            return new Promise((resolve, reject) => {
+                const params = {
+                    q: data.search,
+                    page: context.state.page,
+                }
+                if (data.sort === 'up' && data.order === 'asc') {
+                    params.order = data.order
+                    params.sort = 'repositories'
+                }
+                if (data.sort === 'down') {
+                    params.sort = 'repositories'
+                }
+                api.get('search/users', {
+                    params: params
+                })
+                    .then((response) => {
+                        context.commit('GET_USERS', response.data)
+                        resolve(response)
+                    })
+                    .catch((error) => {
+                        reject(error)
+                    })
+            })
+        },
+        getSomeUser (context, login) {
+            return new Promise((resolve, reject) => {
+                api.get(`users/${login}`)
+                    .then((response) => {
+                        context.commit('GET_SOME_USER', response.data)
+                        resolve(response)
+                    })
+                    .catch((error) => {
+                        reject(error)
+                    })
+            })
         }
     }
 })
